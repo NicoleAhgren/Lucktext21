86cebf2f689fa7341c081ac9be9dd986c8c1228e
Nicole Åhgren <ahgren.nicole@elev.ga.ntig.se>
2022-08-29 20:01:19 +0200

    first commit

---
+++ Lucktext21/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+string name = "";
+string mat = "";
+string person = "";
+string ställe = "";
+
+Console.WriteLine("Vad heter du?");
+name = Console.ReadLine();
+
+Console.WriteLine("Skriv in en maträtt");
+mat = Console.ReadLine();
+
+Console.WriteLine("Skriv in någon annans namn");
+person = Console.ReadLine();
+
+Console.WriteLine("Skriv in en plats (exempelvis: i en skog, på ett berg)");
+ställe = Console.ReadLine();
+
+Console.WriteLine($"{name} och {person} är {ställe}, de sitter där och äter {mat}");
+
+Console.WriteLine("Tryck på ENTER för att avsluta");
+Console.ReadLine();
+
+
